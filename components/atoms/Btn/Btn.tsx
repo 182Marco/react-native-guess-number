4fe2cb40b10ae59bd7669ef4a21e8b456194c49f
Milza <marcomilza@gmail.com>
2025-04-30 10:51:59 +0200

    updated atom

--- components/atoms/Btn/Btn.tsx
+++ components/atoms/Btn/Btn.tsx
@@ -5,15 +5,17 @@ import * as Gs from '../../../styles';
 
 const Btn: IBtn = p => {
   return (
-    <N.Pressable
-      style={S.btnBoxStyle(p)}
-      onPress={p.onPress}
-      {...Gs.android_ripple}
-    >
-      <N.View style={S.btnStyle(p)}>
-        <N.Text style={S.btnTextStyle(p)}>{p.text}</N.Text>
-      </N.View>
-    </N.Pressable>
+    <N.View style={S.btnBoxStyle(p)}>
+      <N.Pressable
+        style={S.btnBoxStyle(p)}
+        onPress={p.onPress}
+        {...Gs.android_ripple}
+      >
+        <N.View style={S.btnStyle(p)}>
+          <N.Text style={S.btnTextStyle(p)}>{p.text}</N.Text>
+        </N.View>
+      </N.Pressable>
+    </N.View>
   );
 };
 
