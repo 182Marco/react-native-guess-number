87c1922d188e735751b633f48cdbf35c920ec6bb
Milza <marcomilza@gmail.com>
2025-05-02 00:15:19 +0200

    global styles for btns

---
+++ styles/btns.ts
@@ -0,0 +1,10 @@
+import { colors } from './colors';
+
+const btnPrimary = {
+  BgColor: colors.primaryBtn,
+  borderRadius: '28',
+  paddingVertical: '8',
+  paddingHorizontal: '16',
+};
+
+export { btnPrimary };
