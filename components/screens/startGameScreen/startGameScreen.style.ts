ca88a493b8a93e66c715bb3c40d17efed92ed902
Milza <marcomilza@gmail.com>
2025-05-04 02:02:36 +0200

    startGame style  Update

---
+++ components/screens/startGameScreen/startGameScreen.style.ts
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+import * as globalS from '../../../styles';
+
+const S = StyleSheet.create({
+  allScreenBox: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  btnRow: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  inputBox: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: globalS.colors.background,
+    borderRadius: 8,
+    elevation: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    //ios
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    width: 50,
+    textAlign: 'center',
+    fontSize: 32,
+    borderBottomColor: globalS.colors.secondary,
+    borderBottomWidth: 2,
+    color: globalS.colors.secondary,
+    marginVertical: 8,
+    fontWeight: 'bold',
+  },
+});
+
+export { S };
