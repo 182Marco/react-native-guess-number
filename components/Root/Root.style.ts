de790df57bd492317e47bb12d50b7349119fd880
Milza <marcomilza@gmail.com>
2025-05-06 00:09:08 +0200

    updated style in root

---
+++ components/Root/Root.style.ts
@@ -0,0 +1,14 @@
+import { colors } from '@/styles';
+import { StyleSheet } from 'react-native';
+
+const S = StyleSheet.create({
+  bg: {
+    flex: 1,
+    backgroundColor: colors.primary,
+  },
+  img: {
+    opacity: 0.9,
+  },
+});
+
+export { S };
