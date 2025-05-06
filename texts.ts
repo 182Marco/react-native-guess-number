b292d589acb975099026c5396e170046551f90af
Milza <marcomilza@gmail.com>
2025-05-06 11:42:09 +0200

    texts updated

--- texts.ts
+++ texts.ts
@@ -3,7 +3,7 @@ const texts = {
   confirmBtnText: 'Confirm',
   alertNumTitle: 'Ivalid number!',
   alertNumToBigDescription: 'The number must be less than 99',
-
+  alertNumLessThan1: 'The number must be greater than 1',
   btnAlertOkText: 'Ok',
 };
 
