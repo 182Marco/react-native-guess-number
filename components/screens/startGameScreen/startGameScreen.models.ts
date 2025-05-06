faaac996d9281edad255cbd7f07bb8dac2e07d84
Milza <marcomilza@gmail.com>
2025-05-06 14:38:58 +0200

    removed unused model

--- components/screens/startGameScreen/startGameScreen.models.ts
+++ components/screens/startGameScreen/startGameScreen.models.ts
@@ -1,9 +1,5 @@
-interface IExample {
-  text: string;
-}
-
 type IGetAlertMsg = (description: string, fn: () => void) => void;
 
 type IAlertFn = (onPressBtnFn: () => void) => void;
 
-export { IExample, IAlertFn, IGetAlertMsg };
+export { IAlertFn, IGetAlertMsg };
