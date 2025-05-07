9d725511fcd400eb1d381b3f76c7fe1a4ab5ef90
Milza <marcomilza@gmail.com>
2025-05-07 11:06:50 +0200

    updated root logic

---
+++ components/Root/Root.hook.ts
@@ -0,0 +1,20 @@
+import { appScreens } from '@/constants';
+import * as R from 'react';
+import { IIsScreen } from './models';
+
+const useRoot = () => {
+  const [screen, setScreen] = R.useState<appScreens>(
+    appScreens.START_GAME_SCREEN
+  );
+  const [pickedNum, setPickedNum] = R.useState<number>();
+
+  R.useEffect(() => {
+    if (pickedNum) setScreen(appScreens.GAME_SCREEN);
+  }, [pickedNum]);
+
+  const isScreen: IIsScreen = str => screen === str;
+
+  return { screen, setScreen, isScreen, setPickedNum };
+};
+
+export { useRoot };
