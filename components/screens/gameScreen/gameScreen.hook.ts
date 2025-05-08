a9a85a9bba8ae52802c2c08b2892b7684797f8a8
Milza <marcomilza@gmail.com>
2025-05-08 12:32:24 +0200

    game screen logic  update

---
+++ components/screens/gameScreen/gameScreen.hook.ts
@@ -0,0 +1,16 @@
+import * as R from 'react';
+import { IUseGameScreen } from './gameScreen.models';
+import { genRandomBetween } from '@/utils';
+
+const useGameScreen: IUseGameScreen = p => {
+  const initialGuess = genRandomBetween({
+    min: 1,
+    max: 100,
+    exclude: p.pickedNum,
+  });
+  const [currGuess, setCurrGuess] = R.useState(initialGuess);
+
+  return { initialGuess };
+};
+
+export { useGameScreen };
