33ff984f0838d353d8330456af66d220878975fb
Milza <marcomilza@gmail.com>
2025-05-08 12:32:40 +0200

    start game screen models update

--- components/screens/startGameScreen/startGameScreen.models.ts
+++ components/screens/startGameScreen/startGameScreen.models.ts
@@ -1,5 +1,15 @@
+import { ISetScreen } from '@/constants';
+
+interface IStartGameScreenProps {
+  setPickedNum: (n: number) => void;
+}
+
+type IStartGameScreen = React.FC<IStartGameScreenProps>;
+
+type IUseStartGameScreen = (p: IStartGameScreenProps) => Record<string, any>;
+
 type IGetAlertMsg = (description: string, fn: () => void) => void;
 
 type IAlertFn = (onPressBtnFn: () => void) => void;
 
-export { IAlertFn, IGetAlertMsg };
+export { IAlertFn, IGetAlertMsg, IStartGameScreen, IUseStartGameScreen };
