192903f98f8019db1564de8c3c32a396e21bd0ee
Milza <marcomilza@gmail.com>
2025-05-08 12:31:10 +0200

    start game page logic update

--- components/screens/startGameScreen/startGameScreen.hook.ts
+++ components/screens/startGameScreen/startGameScreen.hook.ts
@@ -1,9 +1,30 @@
 import * as R from 'react';
+import { useStartGameScreenAlerts } from './startGameScreen.alerts';
+import { IUseStartGameScreen } from './startGameScreen.models';
+import { appScreens } from '@/constants';
 
-const usestartGameScreen = () => {
+const useStartGameScreen: IUseStartGameScreen = p => {
   const [numberValue, setNumberValue] = R.useState('');
+  const reset = () => setNumberValue('');
 
-  return { numberValue, setNumberValue };
+  const handelConfirm = (n: string) => {
+    const alert = useStartGameScreenAlerts();
+    const num = parseInt(n);
+    if (isNaN(num)) {
+      alert.alertIsNaN(reset);
+    }
+    if (num < 1) {
+      alert.NumLessThan1(reset);
+      return;
+    }
+    if (num > 99) {
+      alert.NumGreaterThan99(reset);
+      return;
+    }
+    p.setPickedNum(num);
+  };
+
+  return { numberValue, setNumberValue, reset, handelConfirm };
 };
 
-export { usestartGameScreen };
+export { useStartGameScreen };
