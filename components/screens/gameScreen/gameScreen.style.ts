d308e9d6f55c3ef8641597816e7d1846bc44ea16
Milza <marcomilza@gmail.com>
2025-05-09 11:21:46 +0200

    updated game screen style

---
+++ components/screens/gameScreen/gameScreen.style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const S = StyleSheet.create({
+  screen: {
+    flex: 1,
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+    borderWidth: 2,
+    borderColor: 'red',
+  },
+});
+
+export { S };
