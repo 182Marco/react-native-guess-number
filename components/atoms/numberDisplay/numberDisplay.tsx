d91d86da6193af3517aab04cf56c25aafbdd9bf4
Milza <marcomilza@gmail.com>
2025-05-10 21:00:14 +0200

    new atom component

---
+++ components/atoms/numberDisplay/numberDisplay.tsx
@@ -0,0 +1,13 @@
+import * as N from 'react-native';
+import { INumberDisplay } from './numberDisplay.models';
+import { S } from './numberDisplay.style';
+
+const NumberDisplay: INumberDisplay = p => {
+  return (
+    <N.View style={S.box}>
+      <N.Text style={S.text}>{p.num}</N.Text>
+    </N.View>
+  );
+};
+
+export { NumberDisplay };
