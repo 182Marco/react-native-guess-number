aed3b0a5a9448875194f0991b6309a2e9530a2f1
Milza <marcomilza@gmail.com>
2025-05-14 00:44:17 +0200

    game over comp created

---
+++ components/screens/gameOverScreen/gameOverScreen.tsx
@@ -0,0 +1,19 @@
+import * as N from 'react-native';
+import * as C from '@/components';
+import { S } from './gameOverScreen.style';
+import { useGameOverScreen } from './gameOverScreen.hook';
+import { IGameOverScreen } from './gameOverScreen.models';
+import { texts } from '@/texts';
+import { appScreens } from '@/constants';
+
+const GameOverScreen: IGameOverScreen = p => {
+  const h = useGameOverScreen(p);
+  return (
+    <N.View>
+      <N.Text style={S.text}>Game Over Screen</N.Text>
+      <C.Btn onPress={h.reset} text={texts.startOverBtn} />
+    </N.View>
+  );
+};
+
+export { GameOverScreen };
