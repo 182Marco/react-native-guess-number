e6916aff3f1b80ea1a53e822b81e3bb78f9fec99
Milza <marcomilza@gmail.com>
2025-05-15 01:36:54 +0200

    gameScreen comp updated

--- components/screens/gameScreen/gameScreen.tsx
+++ components/screens/gameScreen/gameScreen.tsx
@@ -5,7 +5,6 @@ import { texts } from '@/texts';
 import { S } from './gameScreen.style';
 import { useGameScreen } from './gameScreen.hook';
 import { DirEnum, IGameScreen } from './gameScreen.models';
-import { appScreens } from '@/constants';
 
 const GameScreen: IGameScreen = p => {
   const h = useGameScreen(p);
@@ -13,25 +12,19 @@ const GameScreen: IGameScreen = p => {
     <N.View style={S.screen}>
       <C.Title text={texts.gameScreenTitleText} />
       <C.NumberDisplay num={h.currGuess} />
-      <N.Text>Give a right suggestion to the opponent</N.Text>
       <N.View style={S.plusMinusBtnsBox}>
         <C.Btn
           {...Gs.roundedBtn}
           onPress={h.getNewGuess.bind(this, DirEnum.UP)}
-          text="+"
+          text={texts.plusBtnText}
         />
         <C.Btn
           {...Gs.roundedBtn}
           onPress={h.getNewGuess.bind(this, DirEnum.DOWN)}
-          text="-"
+          text={texts.minusBtnText}
         />
       </N.View>
       <N.Text>Round number: {h.round}</N.Text>
-      <C.Btn
-        marginTop="30"
-        onPress={() => p.setScreen(appScreens.START_GAME_SCREEN)}
-        text="get Back"
-      />
     </N.View>
   );
 };
