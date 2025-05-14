a3c9c0ae97b79fe63a39afbcc9e4cf3ee8d922ac
Milza <marcomilza@gmail.com>
2025-05-15 01:35:20 +0200

    new app screen

--- constants/appScreens.ts
+++ constants/appScreens.ts
@@ -1,6 +1,7 @@
 enum appScreens {
   START_GAME_SCREEN = 'START_GAME_SCREEN',
   GAME_SCREEN = 'GAME_SCREEN',
+  GAME_OVER_SCREEN = 'GAME_OVER_SCREEN',
 }
 
 type ISetScreen = (str: appScreens) => void;
