7e5ec7bceae109471e3b2d3e1034967e41f4bfae
Milza <marcomilza@gmail.com>
2025-05-15 01:37:13 +0200

    added utils for gameScreen

--- components/screens/gameScreen/gameScreen.utils.ts
+++ components/screens/gameScreen/gameScreen.utils.ts
@@ -7,16 +7,10 @@ const cheatAlertParams: IAlertParams = [
   [{ text: texts.misLeadingCpuCancelBtn, style: 'cancel' }],
 ];
 
-const pcWinAlertParams: IAlertParams = [
-  'PC HA INDOVINATO',
-  'IL PC HA VINTO',
-  [{ text: 'OK', style: 'cancel' }],
-];
-
 const pcLossAlertParams: IAlertParams = [
   'HAI VINTO',
   'IL PC HA FATTO 3 TENTATIVI SENZA INDOVINARE',
   [{ text: 'OK', style: 'default' }],
 ];
 
-export { cheatAlertParams, pcWinAlertParams, pcLossAlertParams };
+export { cheatAlertParams, pcLossAlertParams };
