06a706f7ca99516808fe220f9fceef7c272191c3
Milza <marcomilza@gmail.com>
2025-05-15 01:37:36 +0200

    added style for gameScreen

--- components/screens/startGameScreen/startGameScreen.style.ts
+++ components/screens/startGameScreen/startGameScreen.style.ts
@@ -2,13 +2,27 @@ import { StyleSheet } from 'react-native';
 import * as globalS from '@/styles';
 
 const S = StyleSheet.create({
+  screen: {
+    flex: 1,
+    marginTop: 60,
+    alignItems: 'center',
+  },
   btnRow: {
+    marginTop: 10,
     flexDirection: 'row',
     justifyContent: 'center',
   },
+  inputExplain: {
+    color: globalS.colors.primaryBtn,
+    fontWeight: 700,
+    fontSize: 24,
+    textAlign: 'center',
+    width: '100%',
+  },
   inputBox: {
-    padding: 16,
-    marginTop: 100,
+    marginTop: 27,
+    paddingVertical: 22,
+    paddingHorizontal: 60,
     marginHorizontal: 24,
     backgroundColor: globalS.colors.primary,
     borderRadius: 8,
@@ -25,11 +39,11 @@ const S = StyleSheet.create({
     width: 50,
     textAlign: 'center',
     fontSize: 32,
+    color: globalS.colors.secondary,
+    marginBottom: 12,
+    fontWeight: 'bold',
     borderBottomColor: globalS.colors.secondary,
     borderBottomWidth: 2,
-    color: globalS.colors.secondary,
-    marginVertical: 8,
-    fontWeight: 'bold',
   },
 });
 
