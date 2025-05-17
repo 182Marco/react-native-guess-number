1c7f6ca3434c65d7f6895c9fa0bfac0b50e69b07
Milza <marcomilza@gmail.com>
2025-05-17 23:46:30 +0200

    unused imports

--- components/atoms/Btn/Btn.style.ts
+++ components/atoms/Btn/Btn.style.ts
@@ -1,6 +1,4 @@
 import { StyleSheet } from 'react-native';
-import * as Gs from '@/styles';
-import { IStyleProps } from './Btn.model';
 
 const S = StyleSheet.create({
   margins: {
@@ -20,13 +18,6 @@ const S = StyleSheet.create({
   btnText: {
     fontWeight: 700,
   },
-});
-
-const btnMargins = (p: IStyleProps) => ({
-  ...S.margins,
-  margin: Number(p.margin || 0),
-  marginTop: Number(p.marginTop || 0),
-  marginBottom: Number(p.marginBottom || 0),
 });
 
 export { S };
