a18699d787df6dc3303b5cb7695d462fe0b6b99b
Milza <marcomilza@gmail.com>
2025-05-18 00:13:07 +0200

    updated btn models

--- components/atoms/Btn/Btn.model.ts
+++ components/atoms/Btn/Btn.model.ts
@@ -1,9 +1,11 @@
+import { CSSColor } from '@/models';
 import { GestureResponderEvent, ViewStyle } from 'react-native';
 
 interface BtnAtomProps {
   margins?: ViewStyle;
   btnStyleProps?: ViewStyle;
   onPress?: ((event: GestureResponderEvent) => void) | null | undefined;
+  rippleColor?: CSSColor;
 }
 
 type IBtn = React.FC<BtnAtomProps>;
