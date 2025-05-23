ac45fdb3f579950786521847d350cc866edc324f
Milza <marcomilza@gmail.com>
2025-05-24 00:30:23 +0200

    title style  updated

--- components/atoms/title/title.style.ts
+++ components/atoms/title/title.style.ts
@@ -1,22 +1,17 @@
 import { StyleSheet } from 'react-native';
-import * as globalS from '@/styles';
+import * as Gs from '@/styles';
 import { ITitleStyle } from './title.model';
 
 const S = StyleSheet.create({
   title: {
     fontWeight: 'bold',
     fontSize: 24,
-    color: globalS.colors.textColor,
+    color: Gs.colors.textColor,
     textAlign: 'center',
     borderWidth: 2,
-    borderColor: globalS.colors.textColor,
+    borderColor: Gs.colors.textColor,
     padding: 12,
   },
 });
 
-const title = (p: ITitleStyle) => ({
-  ...S.title,
-  padding: Number(p.padding) || 12,
-});
-
-export { title };
+export { S };
