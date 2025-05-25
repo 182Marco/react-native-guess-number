ea470e4e4b39a0c1bab73435b800bec81b98a6f7
Milza <marcomilza@gmail.com>
2025-05-25 22:17:29 +0200

    updated style

--- components/screens/gameOverScreen/gameOverScreen.style.ts
+++ components/screens/gameOverScreen/gameOverScreen.style.ts
@@ -1,11 +1,11 @@
-import { colors } from '@/styles';
+import * as Gs from '@/styles';
 import { StyleSheet } from 'react-native';
 
 const S = StyleSheet.create({
   screenWrapper: { marginTop: 60 },
   text: {
     fontSize: 30,
-    color: 'pink',
+    color: Gs.colors.inputBackground,
     width: '80%',
     margin: 'auto',
   },
