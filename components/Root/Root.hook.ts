4a7311a59da280419ab105e84a3fece952d8e1c4
Milza <marcomilza@gmail.com>
2025-05-26 21:48:56 +0200

    updated route logic

--- components/Root/Root.hook.ts
+++ components/Root/Root.hook.ts
@@ -7,14 +7,24 @@ const useRoot = () => {
     appScreens.START_GAME_SCREEN
   );
   const [pickedNum, setPickedNum] = R.useState<number | null>(null);
+  const [round, setRound] = R.useState(1);
 
   R.useEffect(() => {
+    setRound(pv => (pv !== pickedNum ? pv + 1 : pv));
     if (pickedNum) setScreen(appScreens.GAME_SCREEN);
   }, [pickedNum]);
 
   const isScreen: IIsScreen = str => screen === str;
 
-  return { screen, setScreen, isScreen, setPickedNum, pickedNum };
+  return {
+    screen,
+    setScreen,
+    isScreen,
+    setPickedNum,
+    pickedNum,
+    round,
+    setRound,
+  };
 };
 
 export { useRoot };
