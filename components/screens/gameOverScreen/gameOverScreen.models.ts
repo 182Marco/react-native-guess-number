df5202d18bf03fbeadb846bdc2f3ad7d99ff9257
Milza <marcomilza@gmail.com>
2025-05-26 12:25:27 +0200

    game over screen models upadated

--- components/screens/gameOverScreen/gameOverScreen.models.ts
+++ components/screens/gameOverScreen/gameOverScreen.models.ts
@@ -3,6 +3,8 @@ import { ISetScreen } from '@/constants';
 interface IGameOverScreenProps {
   setScreen: ISetScreen;
   setPickedNum: (n: number | null) => void;
+  pickedNum: number | null;
+  guessesNum: number;
 }
 
 type IUseGameOverScreen = (p: IGameOverScreenProps) => Record<string, any>;
