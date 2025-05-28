add2dc451f6c4af5aa79321586901fa0e672e0ef
Milza <marcomilza@gmail.com>
2025-05-28 10:04:34 +0200

    updated game screen utils

--- components/screens/gameScreen/gameScreen.utils.ts
+++ components/screens/gameScreen/gameScreen.utils.ts
@@ -1,6 +1,8 @@
 import { IAlertParams } from '@/models';
 import { texts } from '@/texts';
 import * as Gs from '@/styles';
+import * as Gu from '@/utils';
+import { DirEnum, IGetMinOrMax } from './gameScreen.models';
 
 const cheatAlertParams: IAlertParams = [
   texts.misLeadingCpuTitle,
@@ -19,4 +21,24 @@ const ioniconsIconProps = {
   color: Gs.colors.textColor,
 };
 
-export { cheatAlertParams, ioniconsIconProps, pcLossAlertParams };
+const getMin: IGetMinOrMax = (dir, pv, rounds) => {
+  if (rounds.length === 1 && dir === DirEnum.DOWN) return 0;
+  if (rounds.length === 1 && dir === DirEnum.UP) return pv;
+  const l = Gu.getLower([...rounds, pv]);
+
+  return l;
+};
+
+const getMax: IGetMinOrMax = (dir, pv, rounds) => {
+  if (rounds.length === 1 && dir === DirEnum.UP) return 100;
+  if (rounds.length === 1 && dir === DirEnum.DOWN) return pv;
+  return Gu.getHigher([...rounds, pv]);
+};
+
+export {
+  cheatAlertParams,
+  ioniconsIconProps,
+  pcLossAlertParams,
+  getMin,
+  getMax,
+};
