dd8f671f2162f6e4db3794de4c2efde542eb5bbc
Milza <marcomilza@gmail.com>
2025-05-30 01:08:14 +0200

    updated gameScreen style

--- components/screens/gameScreen/gameScreen.style.ts
+++ components/screens/gameScreen/gameScreen.style.ts
@@ -1,22 +1,30 @@
-import * as Gs from '@/styles';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, useWindowDimensions } from 'react-native';
 
-const S = StyleSheet.create({
-  title: {
-    color: Gs.colors.secondary,
-    borderColor: Gs.colors.secondary,
-  },
-  screen: {
-    flex: 1,
-    paddingVertical: 60,
-    paddingHorizontal: 20,
-  },
-  plusMinusBtnsBox: {
-    marginVertical: 20,
-    flexDirection: 'row',
-    gap: 30,
-    justifyContent: 'center',
-  },
-});
+const useStyles = () => {
+  const { height } = useWindowDimensions();
+  const IsLandskape = height < 380;
 
-export { S };
+  return StyleSheet.create({
+    screen: {
+      flex: 1,
+      paddingVertical: IsLandskape ? 25 : 60,
+      paddingHorizontal: 20,
+    },
+    plusMinusBtnsBox: {
+      marginVertical: 20,
+      flexDirection: 'row',
+      gap: 30,
+      justifyContent: 'center',
+    },
+    displayNumber: {
+      paddingVertical: IsLandskape ? 2 : undefined,
+      marginBottom: IsLandskape ? 8 : undefined,
+    },
+    title: {
+      color: 'black',
+      borderColor: 'black',
+    },
+  });
+};
+
+export { useStyles };
