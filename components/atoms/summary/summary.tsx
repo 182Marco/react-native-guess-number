73aa09e4a3e0b5dfafaa149fbeb9c404c7715811
Milza <marcomilza@gmail.com>
2025-05-29 11:28:32 +0200

    new summary comp

---
+++ components/atoms/summary/summary.tsx
@@ -0,0 +1,15 @@
+import * as N from 'react-native';
+import * as Gs from '@/styles';
+import { S } from './summary.style';
+import { ISummary } from './summary.models';
+
+const Summary: ISummary = p => (
+  <N.Text style={[Gs.texts.summaryText, S.SummaryText]}>
+    {p.text1}
+    <N.Text style={Gs.texts.highlightedText}>{p.times}</N.Text>
+    {p.text2}
+    <N.Text style={Gs.texts.highlightedText}>{p.number}</N.Text>
+  </N.Text>
+);
+
+export { Summary };
