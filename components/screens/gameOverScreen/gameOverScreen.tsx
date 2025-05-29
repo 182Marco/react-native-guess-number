241b32ea4b7dd06084d6db77f41abd0f4c34eeb7
Milza <marcomilza@gmail.com>
2025-05-30 01:07:51 +0200

    updated gameOverScreen

--- components/screens/gameOverScreen/gameOverScreen.tsx
+++ components/screens/gameOverScreen/gameOverScreen.tsx
@@ -1,14 +1,12 @@
 import * as N from 'react-native';
 import * as C from '@/components';
-import * as Gs from '@/styles';
-import { S } from './gameOverScreen.style';
 import { useGameOverScreen } from './gameOverScreen.hook';
 import { IGameOverScreen } from './gameOverScreen.models';
 import { texts } from '@/texts';
 import { winImg } from '@/constants';
 
 const GameOverScreen: IGameOverScreen = p => {
-  const h = useGameOverScreen(p);
+  const { S, ...h } = useGameOverScreen(p);
   return (
     <N.View style={S.screenWrapper}>
       <C.Title style={S.title} text={texts.gameOverScreenTitle} />
@@ -20,6 +18,7 @@ const GameOverScreen: IGameOverScreen = p => {
         number={Number(p.pickedNum)}
         text1={texts.gameOverSummary.part1}
         text2={texts.gameOverSummary.part2}
+        style={S.summary}
       />
       <C.Btn onPress={h.reset} text={texts.startOverBtn} />
     </N.View>
