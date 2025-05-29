36f3bcda4a6307f5020887f0d090fbb61ebb01f9
Milza <marcomilza@gmail.com>
2025-05-29 11:28:42 +0200

    new summary syle

---
+++ components/atoms/summary/summary.style.ts
@@ -0,0 +1,12 @@
+import * as Gs from '@/styles';
+import { StyleSheet } from 'react-native';
+
+const S = StyleSheet.create({
+  SummaryText: {
+    color: Gs.colors.secondary,
+    marginBottom: 0,
+    marginTop: 0,
+  },
+});
+
+export { S };
