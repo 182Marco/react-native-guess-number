448f4ec20c0f0f33b5a798bce75bff33af99b774
Milza <marcomilza@gmail.com>
2025-05-30 01:07:11 +0200

    updated summary models

--- components/atoms/summary/summary.models.ts
+++ components/atoms/summary/summary.models.ts
@@ -1,8 +1,13 @@
-type ISummary = React.FC<{
+import * as N from 'react-native';
+
+interface ISummaryProps {
   times: number;
   number: number;
   text1: string;
   text2: string;
-}>;
+  style?: N.TextStyle;
+}
+
+type ISummary = React.FC<ISummaryProps>;
 
 export { ISummary };
