b5a65309f4271cee3634e96e2d00d32cbfcc65ea
Milza <marcomilza@gmail.com>
2025-05-30 01:07:42 +0200

    updated gameOverScreen style

--- components/screens/gameOverScreen/gameOverScreen.style.ts
+++ components/screens/gameOverScreen/gameOverScreen.style.ts
@@ -1,40 +1,46 @@
 import * as Gs from '@/styles';
+import * as N from 'react-native';
 import { StyleSheet } from 'react-native';
 
-const imageSize = '100%';
-const imageWrapSize = 300;
-const borderRadius = imageWrapSize / 2;
-const screenMainColor = Gs.colors.inputBackground;
+const useStyles = () => {
+  const { height } = N.useWindowDimensions();
+  const IsLandskape = height < 380;
+  const imageSize = '100%';
+  const imageWrapSize = IsLandskape ? 100 : 230;
+  const borderRadius = imageWrapSize / 2;
+  const screenMainColor = Gs.colors.inputBackground;
 
-const S = StyleSheet.create({
-  screenWrapper: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 30,
-  },
-  title: {
-    fontSize: 30,
-    color: screenMainColor,
-    borderColor: screenMainColor,
-    width: '80%',
-    margin: 'auto',
-    marginBottom: 25,
-  },
-  imgWrap: {
-    width: imageWrapSize,
-    height: imageWrapSize,
-    borderRadius: borderRadius,
-    borderWidth: 3,
-    borderColor: screenMainColor,
-    overflow: 'hidden',
-    marginTop: 12,
-  },
-  img: {
-    borderRadius: borderRadius,
-    width: imageSize,
-    height: imageSize,
-  },
-});
+  return StyleSheet.create({
+    screenWrapper: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingHorizontal: 30,
+    },
+    title: {
+      fontSize: 30,
+      color: screenMainColor,
+      borderColor: screenMainColor,
+      width: '80%',
+    },
+    imgWrap: {
+      width: imageWrapSize,
+      height: imageWrapSize,
+      borderRadius: borderRadius,
+      borderWidth: 3,
+      borderColor: screenMainColor,
+      overflow: 'hidden',
+      marginVertical: IsLandskape ? 6 : 30,
+    },
+    img: {
+      borderRadius: borderRadius,
+      width: imageSize,
+      height: imageSize,
+    },
+    summary: {
+      marginBottom: 10,
+    },
+  });
+};
 
-export { S };
+export { useStyles };
