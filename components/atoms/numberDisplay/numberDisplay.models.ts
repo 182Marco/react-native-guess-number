bd37ee428cbefc24c739f53ca592d3548dc7edb7
Milza <marcomilza@gmail.com>
2025-05-30 01:06:50 +0200

    updated number display config

--- components/atoms/numberDisplay/numberDisplay.models.ts
+++ components/atoms/numberDisplay/numberDisplay.models.ts
@@ -1,3 +1,9 @@
-type INumberDisplay = React.FC<{ num: number }>;
+import * as N from 'react-native';
+interface INumberDisplayProps {
+  num: number;
+  style?: N.ViewStyle;
+}
+
+type INumberDisplay = React.FC<INumberDisplayProps>;
 
 export { INumberDisplay };
