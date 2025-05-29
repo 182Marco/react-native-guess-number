bc7fd80bf01ca8744842fc9b93ab317cb7d87357
Milza <marcomilza@gmail.com>
2025-05-30 01:07:19 +0200

    updated summary comp

--- components/atoms/summary/summary.tsx
+++ components/atoms/summary/summary.tsx
@@ -4,7 +4,7 @@ import { S } from './summary.style';
 import { ISummary } from './summary.models';
 
 const Summary: ISummary = p => (
-  <N.Text style={[Gs.texts.summaryText, S.SummaryText]}>
+  <N.Text style={[Gs.texts.summaryText, S.SummaryText, p.style]}>
     {p.text1}
     <N.Text style={Gs.texts.highlightedText}>{p.times}</N.Text>
     {p.text2}
