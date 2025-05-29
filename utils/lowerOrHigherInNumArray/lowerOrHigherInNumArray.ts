d0a86af0db2f4f773a4454cc87995c1c069e6e31
Milza <marcomilza@gmail.com>
2025-05-29 11:32:35 +0200

    updated utility fn

--- utils/lowerOrHigherInNumArray/lowerOrHigherInNumArray.ts
+++ utils/lowerOrHigherInNumArray/lowerOrHigherInNumArray.ts
@@ -1,7 +1,16 @@
-import { IGetNum } from './lowerOrHigherInNumArray.models';
+import { IGetLimit, IGetNum } from './lowerOrHigherInNumArray.models';
 
 const getHigher: IGetNum = ar => ar.reduce((a, c) => (c > a ? c : a));
-
 const getLower: IGetNum = ar => ar.reduce((a, c) => (c < a ? c : a));
 
-export { getHigher, getLower };
+const lowerLimit: IGetLimit = (n, ar) => {
+  const filtered = ar.filter(e => e < n);
+  return filtered.length > 0 ? getHigher(filtered) : 0;
+};
+
+const upperLimit: IGetLimit = (n, ar) => {
+  const filtered = ar.filter(e => e > n);
+  return filtered.length > 0 ? getLower(filtered) : 100;
+};
+
+export { lowerLimit, upperLimit };
