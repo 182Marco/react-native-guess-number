c33ffadd9ca1ada3741129d7285b857cd48bcae1
Milza <marcomilza@gmail.com>
2025-05-31 01:57:07 +0200

    added status bar

--- components/Root/Root.tsx
+++ components/Root/Root.tsx
@@ -7,34 +7,40 @@ import { appScreens, diceImg } from '@/constants';
 const Root: React.FC = () => {
   const h = useRoot();
   return (
-    <N.ImageBackground
-      source={diceImg}
-      resizeMode="cover"
-      imageStyle={S.img}
-      style={S.bg}
-    >
-      <N.SafeAreaView />
-      {h.isScreen(appScreens.START_GAME_SCREEN) && (
-        <C.StartGameScreen setPickedNum={h.setPickedNum} />
-      )}
-      {h.isScreen(appScreens.GAME_SCREEN) && (
-        <C.GameScreen
-          setScreen={h.setScreen}
-          pickedNum={h.pickedNum as number}
-          rounds={h.rounds}
-          setRounds={h.setRounds}
-        />
-      )}
-      {h.isScreen(appScreens.GAME_OVER_SCREEN) && (
-        <C.GameOverScreen
-          setScreen={h.setScreen}
-          pickedNum={h.pickedNum}
-          setPickedNum={h.setPickedNum}
-          rounds={h.rounds}
-          setRounds={h.setRounds}
-        />
-      )}
-    </N.ImageBackground>
+    <>
+      <N.StatusBar
+        barStyle="dark-content"
+        backgroundColor="#ffffff"
+      ></N.StatusBar>
+      <N.ImageBackground
+        source={diceImg}
+        resizeMode="cover"
+        imageStyle={S.img}
+        style={S.bg}
+      >
+        <N.SafeAreaView />
+        {h.isScreen(appScreens.START_GAME_SCREEN) && (
+          <C.StartGameScreen setPickedNum={h.setPickedNum} />
+        )}
+        {h.isScreen(appScreens.GAME_SCREEN) && (
+          <C.GameScreen
+            setScreen={h.setScreen}
+            pickedNum={h.pickedNum as number}
+            rounds={h.rounds}
+            setRounds={h.setRounds}
+          />
+        )}
+        {h.isScreen(appScreens.GAME_OVER_SCREEN) && (
+          <C.GameOverScreen
+            setScreen={h.setScreen}
+            pickedNum={h.pickedNum}
+            setPickedNum={h.setPickedNum}
+            rounds={h.rounds}
+            setRounds={h.setRounds}
+          />
+        )}
+      </N.ImageBackground>
+    </>
   );
 };
 
